f8641754b2bf97e5dad35be4907b14c4bee3c2a3
Heiðar Sigurjónsson <heidars19@ru.is>
2019-11-27 19:12:27 +0000

    F5 Morgunfundur 27/11, lagfærði nafn á F3 fundi

--- program_code/zHugmyndir/interface.py
+++ program_code/zHugmyndir/interface.py
@@ -1,25 +1,26 @@
 
+BACKGROUND = "\033[0;37;40m"
+print(BACKGROUND) # Sets font to white (37) and background to black (40)
 
 
-AIRPLANE = ("   __|__   ","--@-(_)-@--"," !  !  !  ")
-AIRPORT = '\n\
+AIRPLANE = ("\033[0;32;40m   __|__   ","--@-(_)-@--"," !  !  !  "+BACKGROUND)
+AIRPORT = '\033[0;35;40m\n\
   _\n\
  | |\                                                         ===================\n\
 ===| \______________|_______                                  |  NaNTMAirTMHQ   |\n\
   \|  O ooooo-=====-|ooo O n\__                               |      _____      |\n\
     -|=|--o-----||--|---|=||---’  _o’     o   .        o o    | $$  |  |  |  $$ |\n\
-_____|=|__|\___oo______|=|o________|_____|\   |     ___|\|\__ |_____|  |  |_____|'
+_____|=|__|\___oo______|=|o________|_____|\   |     ___|\|\__ |_____|  |  |_____|'+BACKGROUND
 
-AIRPORT2 = '\n\
+AIRPORT2 = '\033[0;35;40m\n\
                                             __|__\n\
   _                                     ---@-(")-@---\n\
  | |\                                      !  !  !            ===================\n\
 ===| \______________|_______                                  |  NaNTMAirTMHQ   |\n\
   \|  O ooooo-=====-|ooo O n\__            . . . .            |      _____      |\n\
     -|=|--o-----||--|---|=||---’  _o’     o   .        o o    | $$  |  |  |  $$ |\n\
-_____|=|__|\___oo______|=|o________|_____|\   |     ___|\|\__ |_____|  |  |_____|'
+_____|=|__|\___oo______|=|o________|_____|\   |     ___|\|\__ |_____|  |  |_____|'+BACKGROUND
 
-print("\033[0;37;40m") # Sets font to white (37) and background to black (40)
 
 def menu_box (option, menu_object, length) :
     buffer_list = []
@@ -29,22 +30,26 @@ def menu_box (option, menu_object, length) :
     return menu_string
 
 
-def make_user_interface(interface_list):
-    
+def make_user_interface(interface_list, menu_pos):
+    menu_number = len(menu_pos)
     header_string = "+-----------------------+"
     userface_list = []
     interface_string = ""
-    for i in range(len(interface_list)):
+    for i in range(len(interface_list)): # Iters over the picked menu and the shows the next options 
         buffer_list = []
     
         for x in range(len(interface_list[i])):
             buffer_list.append(menu_box(x+1, interface_list[i][x], len(header_string)-2))
         userface_list.append(buffer_list)
+
     for i in range(len(AIRPLANE)):
         interface_string += "{:>{lengd:}}".format(AIRPLANE[i],lengd = (len(header_string)-2) * len(userface_list) ) + "\n"
+
     for i in range(len(userface_list)):
         interface_string += header_string + " "
+
     interface_string += "\n"
+
     for i in range(len(userface_list)):
         for x in range(len(userface_list[i])):
             for y in range(len(userface_list)):
@@ -72,12 +77,13 @@ def select_menu():
             print("Wrong selection")
 
 
-MENU1 = ("\033[0;31;40mDeildarstjóri \033[0;37;40m ", "Vaktstjóri", "Quit")
-MENU2 = ("Max","*Boeing*","Back")
-MENU3 = ("*Nuuk*","Þórshöfn","Back")
+MENU1 = ("\033[0;31;40mDeildarstjóri"+BACKGROUND, "Vaktstjóri", "Quit")
+MENU2 = ("Max","Boeing","Back")
+MENU3 = ("Nuuk","Þórshöfn","Back")
+chosen_menu = [["Deildarstjóri"]]
 def main():
     interface_list = (MENU1, ) # Skilja eftir kommu til að fá ekki villu ;)
-    interface_string = make_user_interface(interface_list)
+    interface_string = make_user_interface(interface_list, chosen_menu)
     print(interface_string)
     """
     if select_menu():
