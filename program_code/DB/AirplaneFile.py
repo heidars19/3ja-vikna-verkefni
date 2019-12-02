5001da0552cf20a43dbf26fd077cf466cc56da1d
Heiðar Sigurjónsson <heidars19@ru.is>
2019-12-02 16:21:09 +0000

    laga gitignore

---
+++ program_code/DB/AirplaneFile.py
@@ -0,0 +1,3 @@
+class AirplaneFile (ContactFile) :
+    
+    pass
--- program_code/DB/ContactFile.py
+++ program_code/DB/ContactFile.py
@@ -1,3 +1,19 @@
 class ContactFile :
     
+    ''' Abstract class for filehandling '''
+    
+    def openFile(filename) :
+        with open(filename, "r") as newfile :
+            return newfile
+    
+    def __init__ (self, filename) :
+        filestream = openFile(filename)
+        
+    
+    
+    
+    
+    
+    
+    
     pass
