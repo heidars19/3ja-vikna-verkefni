b50f12c9e421dee6e976ddd490bf113faf76598d
eythorb19 <eythorb19@ru.is>
2019-12-02 21:35:20 +0000

    Klasar í LL
    Klasar stofnaðir - ásamt LL-klösum hugsaðir sem logic fyrir hvern klasa. Á eftir að ákveða betur strúktúrinn á þessu.

---
+++ program_code/LL/Class_Definitions.py
@@ -0,0 +1,35 @@
+class StaffMember:
+    def __init__(self,socialsecurity=""):
+
+        self.name = ""
+        self.address = ""
+        self.email = ""
+        self.homephone = ""
+        self.cellphone = ""
+        self.socialsecurity = socialsecurity
+
+    def append_data_to_file(self,datalist,socialsecurity,header):
+        pass
+    def readfile(self, data):
+        pass
+    def changefile(self,data):
+        pass
+    
+    
+class Pilot(Staffmember):
+    def __init__(self,licence):
+        pass
+class FlightAttendant(Staffmember):
+    pass
+
+
+class Worktrip:
+    pass
+
+
+class Destination:
+    self.
+
+#þurfum leiðir til að vita hvað displayið er að biðja um þegar við búum til tilvik af pilot/flightattendant
+
+
