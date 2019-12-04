fa775626d2cdfc7ff4949b3a1960682c602a78f9
Sigurgeir Helgason <sigurgeir.helgason@gmail.com>
2019-12-04 17:10:57 +0000

    Vinna med airplanes og destionations

---
+++ program_code/LL/AirplanesLL.py
@@ -0,0 +1,17 @@
+from DB.DATA_API import *
+
+def get_available_in_database():
+    all_plane = get_available_in_database()
+    print (all_plane)
+
+
+def get_register_in_database():
+    all_planes = AirplaneFile.start()
+    return all_planes
+
+def register_to_database():
+    pass
+
+def change_register_in_database():
+    pass
+
---
+++ program_code/LL/DestinationLL.py
@@ -0,0 +1,7 @@
+from DB.DATA_API import *
+
+def register_to_database():
+    pass
+
+def change_register_in_database():
+    pass
