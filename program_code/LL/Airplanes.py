383b2410a9951a7cd7ad8ba560395f61d0067def
Sigurgeir Helgason <sigurgeir.helgason@gmail.com>
2019-12-05 15:47:13 +0000

    Sameina LL og Klasa fael

--- program_code/LL/Airplanes.py
+++ program_code/LL/Airplanes.py
@@ -12,3 +12,43 @@ class Airplane():
 
     def __str__(self):
         return f'{self.planeID},{self.planeType},{self.name},{self.manufacturer},{self.model},{self.name},{self.capacity}'
+
+
+
+        #====================================Functions==========================================#
+
+    def filter_available(all_planes):
+        pass
+
+    def filter_airplanes(planes_list, a_type):
+        header = ['plane_id', 'plane_type', 'manufacturer', 'model', 'name', 'capacity']
+        type_input = None
+        for index, value in enumerate(header):
+            if value.lower() == a_type.lower():
+                type_input = index
+            
+        types = []
+        for list in planes_list:
+            list = list.split(',')
+            if list[index] not in types:
+                types.append(list[3])
+            
+    def get_airplane_list():
+        {PlaneFilehandler = AirplaneFile()
+        all_planes = PlaneFilehandler.start()
+        return all_planes
+
+    def save_airplane(planeID, planeType, manufacturer, model, name, capacity):
+        new_plain = Airplane(planeID, planeType, manufacturer, model, name, capacity)
+        log_plain = AirplaneFile(data_to_append=str(new_plain))
+        log_plain.start()
+
+    def change_airplane(planeID, planeType, manufacturer, model, name, capacity):
+        pass
+
+    def change_employee(self,old_info,new_info):
+            '''Checks if user is trying to change invalid information'''
+            
+            ssn,name,address,mobile,email,role,rank,licence = new_info.split(',')
+
+            old_info = StaffFile(fieldname="ssn",searchparam=ssn)
--- program_code/LL/LL_API.py
+++ program_code/LL/LL_API.py
@@ -2,9 +2,7 @@ from Employee import *
 from EmployeeLL import *
 #from DB.DATA_API import * eigum ekki að tala beint við db
 from Airplanes import *
-from AirplanesLL import *
 from Destination import *
-from DestinationLL import *
 
 
 #Staff functions
@@ -12,31 +10,45 @@ from DestinationLL import *
 def create_employee(ssn,name,address,mobile,email,role,rank,licence):
     """Creates a new employee, returns updated list of employees."""      
     
-    new_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
-    new_emp.save_employee()
-    updated_list = Employee.get_employee_list
+    EmployeeLL.save_employee(ssn,name,address,mobile,email,role,rank,licence)
+
+    # new_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
+    updated_list = EmployeeLL.get_employee_list()
     return updated_list
-    
+
 def change_employee(self,ssn,name,address,mobile,email,role,rank,licence):
     """Changes information about employee, except ssn, name or creation date."""
+    #old_info = StaffFile(fieldname="ssn",searchparam=ssn)
+    new_info = Employee(ssn,name,address,mobile,email,role,rank,licence)
 
-    changed_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
-    changed_emp.change_employee()
-    updated_list = Employee.get_employee_list
-    return updated_list
+    EmployeeLL.change_info(old_info,new_info)
+
+    #old_info = StaffFile(fieldname="ssn",searchparam=ssn)
+    line_number = old_info.run_me()
+
+    data_string = ",".join([ssn,name,address,mobile,email,role,rank,licence])
+
+    new_info = StaffFile(line_to_replace=line_number,replace_with=data_string)
+    new_info.run_me()
+
 
 #----------------------------------------------------------------------------------------------
 
 #Airplanes
 
 def get_airplane_types():
-    all_planes = get_all_planes()
-    filtered_planes = filter_planes(all_planes, model)
+    all_planes = Airplane.get_airplane_list()
+    filtered_planes =Airplane.filter_planes(all_planes, model)
     return filtered_planes
 
-def return_all_airplanes():
+def get_all_airplanes():
     all_planes = get_all_planes()
     return all_planes
+
+def create_airplane(planeID, planeType, manufacturer, model, name, capacity):
+    save_airplane(planeID, planeType, manufacturer, model, name, capacity)
+    
+    
 
 
 
