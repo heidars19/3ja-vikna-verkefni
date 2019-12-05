a2ef454774081c6cda587454cb05141c04f46a6d
eythorb19 <eythorb19@ru.is>
2019-12-05 15:41:48 +0000

    Employee.py updated með nyjum functions

--- program_code/LL/Employee.py
+++ program_code/LL/Employee.py
@@ -1,9 +1,8 @@
 
-import DB.DATA_API
+from DB.DATA_API import *
 
 class Employee:
     """This class represents the Employees of Nan Air and keeps track of their information."""
-
 
     def __init__(self,ssn,name,address,mobile,email,role,rank,licence): 
         from datetime import date
@@ -17,7 +16,7 @@ class Employee:
         self.role = role
         self.rank = rank
         self.licence = licence
-        self.__creationdate = today
+        self.__creation_date = today
 
     def __str__(self):
         return f'{self.__ssn},{self.__name},{self.address},{self.mobile},{self.email},{self.role},{self.rank},{self.licence},{self.__creationdate}'
@@ -34,25 +33,56 @@ class Employee:
         return self.__name
     @property
     def creationdate(self):
-        return self.__creationdate
+        return self.__creation_date
 
     #name setter fyrir privat breytur
-    @ssn.setter 
-    def ssn(self,ssn):
-        self.__ssn = ssn
+    # @ssn.setter 
+    # def ssn(self,ssn):
+    #     self.__ssn = ssn
 
-    @name.setter
-    def name(self,name):
-        self.__name = name
+    # @name.setter
+    # def name(self,name):
+    #     self.__name = name
     
-    @creationdate.setter
-    def creationdate(self,creationdate):
-        self.__creationdate = creationdate
+    # @creationdate.setter
+    # def creationdate(self,creation_date):
+    #     self.__creation_date = creation_date
 
     @classmethod
     def from_string(cls,emp_string):
         ssn,name,address,mobile,email,role,rank,licence = emp_string.split(',')
         return cls(ssn,name,address,mobile,email,role,rank,licence)
+
+#---------------------------------FUNCTIONS FOR EMPLOYEES-----------------------------------------------------#
+
+    def save_employee(self):
+        '''Save new employee to database'''
+        log_emp = StaffFile(data_to_append=str(self))
+        log_emp.start()
+
+    #ABSTRACT?
+    def get_employee_list():    
+        '''Get current list of employees''' 
+        new_emp_list = StaffFile()               #create new instance of list
+        updated_list = new_emp_list.start()      #get contents of list
+        return updated_list                      #return list
+
+    def change_employee(self):
+        '''Changes personal information about employee, except ssn, name and creation_date'''
+
+        line_number = StaffFile(fieldname="ssn",searchparam=self.ssn) #Looks for ssn in StaffFile and returns line number
+        update_line = StaffFile(line_to_replace=line_number,replace_with=self)
+        update_line.start()
+        
+    
+
+
+
+
+
+
+
+
 
 
 new_emp = '2501952149,Eyþór Óli Borgþórsson,Þingás 31,8453474,eythoroli95@gmail.com,Pilot,Copilot,Fokker232'
--- program_code/LL/LL_API.py
+++ program_code/LL/LL_API.py
@@ -7,33 +7,23 @@ from Destination import *
 from DestinationLL import *
 
 
-
 #Staff functions
 #----------------------------------------------------------------------------------------------
 def create_employee(ssn,name,address,mobile,email,role,rank,licence):
     """Creates a new employee, returns updated list of employees."""      
     
-    EmployeeLL.save_employee(ssn,name,address,mobile,email,role,rank,licence)
-
-    # new_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
-    updated_list = EmployeeLL.get_employee_list()
+    new_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
+    new_emp.save_employee()
+    updated_list = Employee.get_employee_list
     return updated_list
-
+    
 def change_employee(self,ssn,name,address,mobile,email,role,rank,licence):
     """Changes information about employee, except ssn, name or creation date."""
-    #old_info = StaffFile(fieldname="ssn",searchparam=ssn)
-    new_info = Employee(ssn,name,address,mobile,email,role,rank,licence)
 
-    EmployeeLL.change_info(old_info,new_info)
-
-    #old_info = StaffFile(fieldname="ssn",searchparam=ssn)
-    line_number = old_info.run_me()
-
-    data_string = ",".join([ssn,name,address,mobile,email,role,rank,licence])
-
-    new_info = StaffFile(line_to_replace=line_number,replace_with=data_string)
-    new_info.run_me()
-
+    changed_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
+    changed_emp.change_employee()
+    updated_list = Employee.get_employee_list
+    return updated_list
 
 #----------------------------------------------------------------------------------------------
 
@@ -44,14 +34,9 @@ def get_airplane_types():
     filtered_planes = filter_planes(all_planes, model)
     return filtered_planes
 
-def get_all_airplanes():
+def return_all_airplanes():
     all_planes = get_all_planes()
     return all_planes
-
-def create_airplane(planeID, planeType, manufacturer, model, name, capacity):
-    save_airplane(planeID, planeType, manufacturer, model, name, capacity)
-    
-    
 
 
 
