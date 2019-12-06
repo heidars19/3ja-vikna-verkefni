1c1891d1e1120b72e04ec909002167c589bd1062
Heiðar Sigurjónsson <heidars19@ru.is>
2019-12-06 19:50:55 +0000

    headers

--- program_code/headers.py
+++ program_code/headers.py
@@ -1,15 +1,15 @@
-AIRPLANE_TABLE = "Data/Airplane.csv"
-    AIRLPANE_TABLE_HEADER = 'plane_id,plane_type,manufacturer,model,name,capacity,registration_date'
+    AIRPLANE_TABLE = "Data/Airplane.csv"
+    AIRLPANE_TABLE_HEADER = 'id,plane_id,plane_type,manufacturer,model,name,capacity,registration_date'
     
     STAFF_TABLE = "Data/Crew.csv"
-    STAFF_TABLE_HEADER = 'ssn,name,address,mobile,email,role,rank,licence,registration_date'
+    STAFF_TABLE_HEADER = 'id,ssn,name,address,mobile,email,role,rank,licence,registration_date'
     
     DESTINATION_TABLE = "Data/Destinations.csv"
     DESTINATION_TABLE_HEADER = 'id,destination,country,flight_time,distance,contact,emerg_number,airport,registration_date'
     
     WORKTRIP_TABLE = "Data/Worktrips.csv"
-    WORKTRIP_TABLE_HEADER = 'id,flight_number_out,flight_number_home,departing_from,arriving_at,departure,arrival,aircraft_id,captain,copilot,fsm,fa1,fa2,registration_date'
+    WORKTRIP_TABLE_HEADER = 'id,flight_number_out,flight_number_home,departing_from,arriving_at,departure,arrival,aircraft_id,captain,copilot,fsm,fa1,fa2,stada,registration_date'
 
     WORKTRIP_OLD_TABLE = "Data/Worktrips_old.csv"
-    WORKTRIP_OLD_TABLE_HEADER = 'id,flight_number_out,flight_number_home,departing_from,arriving_at,departure,arrival,aircraft_id,captain,copilot,fsm,fa1,fa2,registration_date'
+    WORKTRIP_OLD_TABLE_HEADER = 'id,flight_number_out,flight_number_home,departing_from,arriving_at,departure,arrival,aircraft_id,captain,copilot,fsm,fa1,fa2,registration_date,stada,archive_date'
     
