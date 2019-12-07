a5947d9fcf27e9ead6239d8122805f077c2c3753
Heidars19 <heidar.sigurjons@gmail.com>
2019-12-07 14:55:09 +0000

    l

--- program_code/test_worktripfile.py
+++ program_code/test_worktripfile.py
@@ -13,19 +13,19 @@ def main ():
     # print(datalist)
     # print()
     
-    #Append data to a file - check
-    new_instance = WorkTripFile(data_to_append="NA5638,Keflavik,Longyearbyen,2019-12-20T06:45:00,2019-12-20T10:45:00,321321,321321,321323213211,321321321,3213213,321,321,321")
-    new_instance.start()
+    # #Append data to a file - check
+    # new_instance = WorkTripFile(data_to_append="NA5638,Keflavik,Longyearbyen,2019-12-20T06:45:00,2019-12-20T10:45:00,321321,321321,321323213211,321321321,3213213,321,321,321")
+    # new_instance.start()
     
     # # Finds line number of a given search parameter - check
-    # new_instance = WorkTripFile(fieldname="id", searchparam="3")
+    # new_instance = WorkTripFile(fieldname="id", searchparam="22")
     # line_number = new_instance.start()
     # print("Ætti að skila línu númeri: --> {}".format(line_number))
     # print()
     
-    # # Changes a line - check
-    # new_instance = WorkTripFile(line_to_replace="8,Þetta ætti að breyta línu 8", replace_with="22,NA5638,Keflavik,Longyearbyen,2019-12-20T06:45:00,2019-12-20T10:45:00,321321,321321,321323213211,321321321,3213213,321,321,321")
-    # new_instance.start()   
+    # Changes a line - check
+    new_instance = WorkTripFile(line_to_replace="31,NA5638,Keflavik,Longyearbyen,2019-12-20T06:45:00,2019-12-20T10:45:00,321321,321321,321323213211,321321321,3213213,321,321,321,2019-12-07 13:55:18.999874", replace_with="34,NA5638,Keflavik,Longyearbyen,2019-12-20T06:45:00,2019-12-20T10:45:00,321321,321321,321323213211,321321321,3213213,321,321,321,2019-12-07 13:55:26.679896")
+    new_instance.start()   
     
     # # Change a line at a certain line number - check
     # new_instance = WorkTripFile(line_to_replace=6, replace_with="15,NA5638,Keflavik,Longyearbyen,2019-12-20T06:45:00,2019-12-20T10:45:00,321321,321321,321323213211,321321321,3213213,321,321,321")
