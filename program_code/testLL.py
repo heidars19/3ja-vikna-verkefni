37c6a246ee83b6254daaea42ded3e2c4e7cb0727
eythorb19 <eythorb19@ru.is>
2019-12-07 20:37:06 +0000

    fix

--- program_code/testLL.py
+++ program_code/testLL.py
@@ -9,7 +9,7 @@ def main():
   #-----------------------------KEYRSLUTEST------------------------------"
  
   #CREATE
-  # new_instance = LL_API()
+  new_instance = LL_API()
   # return_value = new_instance.create("employee",('','5665552222','Lára','Irmagata 31','0934958','lara@gmail.com','Pilot','Copilot','Airmax'))
   #return_value = new_instance.create("destination", ('','Vancouver', 'Canada','6:30:10','2.100',' John Philips','0219933884','BC_airport'))
   #return_value = new_instance.create("airplane", ('',"TF-TEST","NANTES146","Fokker","555","Skvis"))
