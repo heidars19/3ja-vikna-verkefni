792d2e644135ec8e39faf01b3854e5a8784119c1
Sigurgeir Helgason <sigurgeir.helgason@gmail.com>
2019-12-09 15:24:16 +0000

    Model breytt i capacity

--- program_code/LL/Airplanes.py
+++ program_code/LL/Airplanes.py
@@ -1,18 +1,18 @@
 class Airplanes():
-    def __init__(self, _id,plane_id, plane_type, manufacturer, model, name, registration_date=''):
+    def __init__(self, _id,plane_id, plane_type, manufacturer, capacity, name, registration_date=''):
         self._id = _id
         self.plane_id = plane_id
         self.plane_type = plane_type
         self.manufacturer = manufacturer 	
-        self.model = model
+        self.capacity = capacity
         self.name = name
         self.registration_date = registration_date
 
     def __repr__(self):
-        return f'Airplanes({self._id},{self.plane_id},{self.plane_type},{self.manufacturer},{self.model},{self.name},{self.registration_date})'
+        return f'Airplanes({self._id},{self.plane_id},{self.plane_type},{self.manufacturer},{self.capacity},{self.name},{self.registration_date})'
 
     def get_registration_str(self):
-        return f'{self.plane_id},{self.plane_type},{self.manufacturer},{self.model},{self.name}'
+        return f'{self.plane_id},{self.plane_type},{self.manufacturer},{self.capacity},{self.name}'
 
     def get_changes_registration_str(self):
-        return f'{self._id},{self.plane_id},{self.plane_type},{self.manufacturer},{self.model},{self.name},{self.registration_date}'
+        return f'{self._id},{self.plane_id},{self.plane_type},{self.manufacturer},{self.capacity},{self.name},{self.registration_date}'
