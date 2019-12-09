4ac9e58b00af4b15bfec9d2454311cb1006dee05
Heiðar Sigurjónsson <heidars19@ru.is>
2019-12-09 20:18:14 +0000

    ErrorCheck class finished

--- program_code/random.py
+++ program_code/random.py
@@ -1,25 +1,55 @@
 
-def flight_number_today():
-    
-    
-    return "20"
 
-def get_destination_number() :
-    
-    return "0"
+new_list = [["Sigurgeir Helgason","Flugmaður","Boeing 747","Laus",""],
+                ["Arnar Ívarsson","Flugþjónn","","Í ferð","New York"],
+                ["Sigurgeir Helgason","Flugmaður","Boeing 747","Laus",""],
+                ["Arnar Ívarsson","Flugþjónn","","Í ferð","New York"],
+                ["Arnar Ívarsson","Flugþjónn","","Í ferð","New York"],
+                ["Sigurgeir Helgason","Flugmaður","Boeing 747","Laus",""],
+                ["Arnar Ívarsson","Flugþjónn","","Í ferð","New York"],
+                ["Sigurgeir Helgason","Flugmaður","Boeing 747","Laus",""]
+                ]
 
 
-def flugnumer() :
-    FLUGFELAG = "NA"
-    destination_number = "20" # tveggja stafa númer
-    flug_nr_dag = "" # Númer flugs þennan dag til þessa áfangastaðar, hometrip alltar +1
-    # TD NA200 og heimferð NA201
-    
-    destination_number = get_destination_number()
-    flug_nr_dag = flight_number_today()    
-    
-    
-    return FLUGFELAG + destination_number + flug_nr_dag
+def body_template_func(new_list) :
+    template_list = []
+    template_list.append(("║ ┌────────────────────────────────────────────────────────────────────────────────────────────────────┐ ║"))
+    for i in range(15):
+        temp_tuple = (("║ │"), (new_list[i]),("│ ║") )
+        template_list.append( temp_tuple )
+    template_list.append(("║ └────────────────────────────────────────────────────────────────────────────────────────────────────┘ ║"))
+    return tuple(template_list)
     
     
+            
+
+body_template = (
+(("║ ┌────────────────────────────────────────────────────────────────────────────────────────────────────┐ ║")),
+(("║ │"),(new_list[0]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[1]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[2]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[3]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[4]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[5]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[6]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[7]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[8]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[9]),                                                                                 ("│ ║")),
+(("║ │"),(new_list[10]),                                                                                ("│ ║")),
+(("║ │"),(new_list[11]),                                                                                ("│ ║")),
+(("║ │"),(new_list[12]),                                                                                ("│ ║")),
+(("║ │"),(new_list[13]),                                                                                ("│ ║")),
+(("║ │"),(new_list[14]),                                                                                ("│ ║")),
+(("║ └────────────────────────────────────────────────────────────────────────────────────────────────────┘ ║"))
+)
+
+
+
+result = body_template_func(new_list)
+
+#print(result)
+# for line in result:
+#     print(line)
     
+    
+print(body_template)
