e8d9650960fc982c535c399bffa68b49fd24ccd8
Heiðar Sigurjónsson <heidar@fss.is>
2019-12-10 11:45:46 +0000

    worktripll in progress

--- program_code/testLL.py
+++ program_code/testLL.py
@@ -25,7 +25,7 @@ def main():
   # return_value = new_instance.create("employee",('','5665552222','Eyþór Óli','Irmagata 31','0934958','lara@gmail.com','Pilot','Copilot','Airmax'))
   # return_value = new_instance.create("destination", ('','Toronto', 'Canada','6:30:10','2.100','John Philips','0219933884','BC_airport'))
   # return_value = new_instance.create("airplane", ('',"TF-TEST","NANTES146","Fokker","555","Skvis"))
-  # return_value = new_instance.create("worktrip",('','NA12345','NA12346','Reykjavík','Köben','2019-12-20T06:45:00','2019-12-20T10:45:00','5'))
+  # return_value = new_instance.create("worktrip",('','','','','15','2019-12-20 06:45:00','','5')) # dest_id, departure_time, airplane_id
   # return_msg(return_value, f"creating a new object, code:{return_value}")
 
   #CHANGE
@@ -50,13 +50,13 @@ def main():
 
 
 
-  new_instance = LL_API()
+  # new_instance = LL_API()
   # new_list = new_instance.get_list('employee')
   #new_list = new_instance.get_list('airplane')
   # new_list = new_instance.get_list('destination')
   # new_list = new_instance.get_list('worktrip')
-  new_list = new_instance.get_list('worktrip',"available_employees",'2019-12-19')
-  print(new_list)
+  # new_list = new_instance.get_list('worktrip',"available_employees",'2019-12-19')
+  # print(new_list)
 
 
   # new_instance = EmployeeLL()
