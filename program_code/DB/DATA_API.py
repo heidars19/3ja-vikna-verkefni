d5c88f98d3efe13a6332d04deb6d60ba21a22eca
Heidars19 <heidar.sigurjons@gmail.com>
2019-12-11 13:05:23 +0000

    Commented DATA_API

--- program_code/DB/DATA_API.py
+++ program_code/DB/DATA_API.py
@@ -6,17 +6,21 @@ from DB.WorkTripFile import WorkTripFile
 from DB.WorkTripFileOld import WorkTripFileOld
 
 
-
 class DATA_API:
 
 
     def __init__ (self, keyword='', data_to_append=None, fieldname=None, searchparam=None, line_to_replace=None, replace_with=None, header=False):
+        '''
+        Calls set_sata() to handle initialization
+        '''
         self.set_data(keyword=keyword, data_to_append=data_to_append, fieldname=fieldname, searchparam=searchparam, line_to_replace=line_to_replace, replace_with=replace_with, header=header)
         return
 
+
     def set_data (self, keyword='', data_to_append=None, fieldname=None, searchparam=None, line_to_replace=None, replace_with=None, header=False):
         '''
-        Resets DATA_API
+        Resets DATA_API\n
+        Sets given arguments and resets all others.
         '''
         self.keyword = keyword
         self.data_to_append = data_to_append
@@ -30,6 +34,33 @@ class DATA_API:
 
 
     def start(self):
+        '''
+        Decides what FileHandlr function to run, based on given arguments.
+        First run set_data() to prepare for actuin, then start() to complete it.
+        Can only remember 1 set_sata() at a time.\n
+                
+        # You should only need 1 instance: 
+        data_api = DATA_API()\n
+
+        # Get a list: 
+        data_api.set_data(keyword): \n
+
+        # Append line: 
+        data_api.set_data(keyword, data_to_append="Bætir þessu við neðst í skránna")\n
+
+        # Search by column: 
+        data_api.set_data(keyword, fieldname="id", searchparam="148")\n
+
+        # Replace a line by line-number or a matching previous line: \n
+        data_api.set_data(keyword, line_to_replace="Skiptir út þessari línu", replace_with="Setur þessa línu í staðinn")\n
+        data_api.set_data(keyword, line_to_replace=int, replace_with="Setur þessa línu í línu númer 'int'")\n
+
+        # Runs the command prepared above: 
+        data_api.set_data(keyword, header=True) \n
+
+        possible_return_value = data_api.start() 
+
+        '''
         filename = self.file_type()
         new_instance = filename(data_to_append=self.data_to_append, fieldname=self.fieldname, searchparam=self.searchparam, line_to_replace=self.line_to_replace, replace_with=self.replace_with, header=self.header )
         if self.header :
@@ -57,6 +88,9 @@ class DATA_API:
 
 
     def file_type(self):
+        '''
+        Uses the 'self.keyword' to get filename, so classes can be initialized with that name.
+        '''
 
         file_type = ""
 
