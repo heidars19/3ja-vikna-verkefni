b1c2dd8aa56c6b5d13ad992b631677f9122e9e19
eythorb19 <eythorb19@ru.is>
2019-12-11 15:48:51 +0000

    comments

--- program_code/LL/LL_API.py
+++ program_code/LL/LL_API.py
@@ -6,13 +6,13 @@ from LL.LL_functions import *
 
 class LL_API:
 
-#From UI: create("employee",(ssn,name,address,mobile,email,role,rank,licence))
-    def create(self, keyword,user_input, ):    
-        '''Creates new object and saves to Database. \n
-        keyword: employee,airplane,destination or worktrip
-        \n
-        user_input: user input for corresponding item as tuple. No id or registration date.
+    def create(self, keyword,user_input):    
         '''
+        \n Creates new object and saves to Database. Returns msg with success or error. \n
+        keyword [string]: employee /airplane / destination / worktrip\n
+        user_input [tuple]: User input for corresponding item as tuple with a leading empty string.
+        '''
+
         if keyword == 'employee':
             cr_emp = EmployeeLL()
             run_create = cr_emp.create_employee(user_input)
@@ -29,14 +29,15 @@ class LL_API:
             cr_trip = WorktripLL()
             run_create = cr_trip.create_worktrip(user_input)  
 
-        return run_create
+        return run_create 
 
     def change(self,keyword,changed_object):
-        '''Changes object in Database. \n
-        keyword[str]: employee,airplane,destination or worktrip
-        \n
-        changed_object[tuple]: Changed object, including id and registration date.
         '''
+        \n Changes object in Database. Returns msg with success or error. \n
+        keyword [string]: employee /airplane / destination / worktrip\n
+        changed_object [tuple]: User input for changed object with including id and registration date.
+        '''
+
         if keyword == 'employee':
             ch_emp = EmployeeLL()
             run_change = ch_emp.change_employee(changed_object)
@@ -56,8 +57,9 @@ class LL_API:
         return run_change
 
     def get_list(self,keyword,list_type="",searchparam = "", _id='', role='',rank='', a_license=''):
-            '''Gets updated list from database. \n
-               keyword[str]: employee,airplane,destination,worktrip \n
+            '''
+            Gets updated list from database. \n
+            keyword [string]: employee /airplane / destination / worktrip\n
                 \n
            
                 TO GET LIST OF EMPLOYEES WORKING ON SPECIFIC DATE AND THEIR DESTINATIONS: \n
@@ -91,13 +93,13 @@ class LL_API:
 
             elif list_type == "plane_licences":
                 new_instance = AirplanesLL()
-                plane_licence = new_instance.get_plance_licence(keyword, list_type)
+                plane_licence = new_instance.get_plane_licence()
 
                 return plane_licence
             
             elif list_type == "workschedule":
                 new_instance = WorktripLL()
-                workschedule = new_instance.get_workschedule( searchparam,_id ) #searchparam is the date, the _id is the staffmemebers id.
+                workschedule = new_instance.get_workschedule(searchparam,_id ) #searchparam is the date, the _id is the staffmemebers id.
                 return workschedule
 
 
@@ -112,72 +114,3 @@ class LL_API:
 def testmain():
     new = LL_API()
    
-#    new.create('airplane', ('32','rass','re re','re 31','er','asdf@gmail.com')) #airplane nyskraning test
-#    new.create('employee', ('0','4455668855','aparassgat Helga','Hehe 31','8453474','eythoroli95@gmail.com','Pilot','Copilot','Fokker232'))
-    new.create('destination',('11','asdf','Greenland','1:00:55','1.343','Hraði Brunann','328738975','flugvolllur'))
-
-
-
-
-
-
-
-
-
-
-
-
-# #Staff functions
-# #----------------------------------------------------------------------------------------------
-# def create_employee(ssn,name,address,mobile,email,role,rank,licence):
-#     """Creates a new employee, returns updated list of employees."""      
-    
-#     EmployeeLL.save_employee(ssn,name,address,mobile,email,role,rank,licence)
-
-#     # new_emp = Employee(ssn,name,address,mobile,email,role,rank,licence)
-#     updated_list = EmployeeLL.get_employee_list()
-#     return updated_list
-
-# def change_employee(self,ssn,name,address,mobile,email,role,rank,licence):
-#     """Changes information about employee, except ssn, name or creation date."""
-#     #old_info = EmployeeFile(fieldname="ssn",searchparam=ssn)
-#     new_info = Employee(ssn,name,address,mobile,email,role,rank,licence)
-
-#     EmployeeLL.change_info(old_info,new_info)
-
-#     #old_info = EmployeeFile(fieldname="ssn",searchparam=ssn)
-#     line_number = old_info.run_me()
-
-#     data_string = ",".join([ssn,name,address,mobile,email,role,rank,licence])
-
-#     new_info = EmployeeFile(line_to_replace=line_number,replace_with=data_string)
-#     new_info.run_me()
-
-
-# #----------------------------------------------------------------------------------------------
-# #Airplanes
-
-# def get_airplane_types():
-#     all_planes = Airplane.get_airplane_list()
-#     filtered_planes =Airplane.filter_planes(all_planes, capacity)
-#     return filtered_planes
-
-# def get_all_airplanes():
-#     all_planes = get_all_planes()
-#     return all_planes
-
-# def create_airplane(planeID, planeType, manufacturer, capacity, name, capacity):
-#     save_airplane(planeID, planeType, manufacturer, capacity, name, capacity)
-    
-    
-# #if __name__ == "__main__":
-
-
-#     #main()
-
-
-
-# #new_emp = '2501952149,Eyþór Óli Borgþórsson,Þingás 31,8453474,eythoroli95@gmail.com,Pilot,Copilot,Fokker232'
-# emp = create_employee("2501952149","Eyþór Óli Borgþórsson","Þingás 31","8453474","eythoroli95@gmail.com","Pilot","Copilot","Fokker232")
-
-# print(emp)
