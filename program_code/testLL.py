a5fe438192e4d9c509b702b666b5dadd0377afaf
Sigurgeir Helgason <sigurgeir.helgason@gmail.com>
2019-12-13 15:18:45 +0000

    laga stafsetttningarvilu

--- program_code/testLL.py
+++ program_code/testLL.py
@@ -67,7 +67,7 @@ def main():
   #Work Schedule:  
   #new_list = new_instance.get_list('worktrip', 'work_schedule', '2019-12-28', '', days=1) #fær öll flug fyrir einn dag ákveðna dagsetningu
   #new_list = new_instance.get_list('worktrip', 'work_schedule', '2019-12-19') #fær flug fyrir öll flug áveðna viku
-  #new_list = new1_instance.get_list('worktrip', 'work_schedule', '2019-12-28', _id='23') #fær flug fyrir ákveðinn starfsmann ákveðna viku
+  new_list = new_instance.get_list('worktrip', 'work_schedule', '2019-12-18', _id='1') #fær flug fyrir ákveðinn starfsmann ákveðna viku
 
   #Worktrip translator:
   #new_list = new_instance.get_list(list_type='worktrip_readable', searchparam=("15",'NA156','NA157','Keflavík','15','2019-12-20 11:50:00','2019-12-20 21:45:00','5','3','2','7','11','15','staffed','2019-12-11 01:51:02.065347'))
@@ -75,7 +75,7 @@ def main():
   #Find pilot with license
   #new_list = new_instance.get_list ('employee', "pilot_licences", a_license='BOEING747')
 
-  new_list = new_instance.get_list("destination","destination_name", "10")
+  #new_list = new_instance.get_list("destination","destination_name", "10")
  
  
   # new_list = new_instance.get_list('airplane','plane_licences') 
